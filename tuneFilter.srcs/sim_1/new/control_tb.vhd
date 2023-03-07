39ae0310acdc0568b67b765905f568c84fffc731
PeekOFarad <stepan.matej99@gmail.com>
2023-03-07 13:05:59 +0100

    test070323

---
+++ tuneFilter.srcs/sim_1/new/control_tb.vhd
@@ -0,0 +1,79 @@
+library IEEE;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+use work.tuneFilter_pkg.all;
+
+entity control_tb is
+end;
+
+architecture bench of control_tb is
+
+  component control
+      Port (  clk, rst, RQ    : in STD_LOGIC;
+              input           : in std_logic_vector(c_data_w-1 downto 0);
+              GNT, RDY, en    : out STD_LOGIC;
+              output          : out std_logic_vector(c_data_w-1 downto 0));
+  end component;
+
+  signal clk, rst, RQ: STD_LOGIC;
+  signal input: std_logic_vector(c_data_w-1 downto 0);
+  signal GNT, RDY, en: STD_LOGIC;
+  signal output: std_logic_vector(c_data_w-1 downto 0);
+
+  constant clock_period: time := 10 ns;
+  signal stop_the_clock: boolean;
+
+begin
+
+  uut: control port map ( clk    => clk,
+                          rst    => rst,
+                          RQ     => RQ,
+                          input  => input,
+                          GNT    => GNT,
+                          RDY    => RDY,
+                          en     => en,
+                          output => output );
+
+  stimulus: process
+  begin
+  
+    -- Put initialisation code here
+
+    rst <= '1';
+    wait for 5 ns;
+    rst <= '0';
+    wait for 5 ns;
+
+    -- Put test bench stimulus code here
+    report "test start";
+    input <= std_logic_vector(to_signed(35,c_data_w));
+    RQ <= '1';
+    wait on GNT;
+    report "data writen";
+    RQ <= '0';
+    wait on RDY;
+    report "data out";
+    assert (output = std_logic_vector(to_signed(35,c_data_w)))
+      report ("wrong output, expected: " & integer'image(35) & "; Value read: " & integer'image(to_integer(signed(output))))
+      severity error;
+    if (output = std_logic_vector(to_signed(35,c_data_w))) then
+      report "Test Successful!";
+    else
+      report "Test Failed!" severity failure;
+    end if;
+
+    ------------- 
+    stop_the_clock <= true;
+    wait;
+  end process;
+
+  clocking: process
+  begin
+    while not stop_the_clock loop
+      clk <= '0', '1' after clock_period / 2;
+      wait for clock_period;
+    end loop;
+    wait;
+  end process;
+
+end;
--- tuneFilter.srcs/sources_1/new/cntrl.vhd
+++ tuneFilter.srcs/sources_1/new/cntrl.vhd
@@ -30,8 +30,10 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity control is
-    Port ( clk, rst, RQ : in STD_LOGIC;
-           GNT, RDY, en : out STD_LOGIC);
+    Port (  clk, rst, RQ    : in STD_LOGIC;
+            input           : in std_logic_vector(c_data_w-1 downto 0);
+            GNT, RDY, en    : out STD_LOGIC;
+            output          : out std_logic_vector(c_data_w-1 downto 0));
 end control;
 
 architecture Behavioral of control is
@@ -43,11 +45,12 @@ signal coeff_mem : t_coeff_mem := (
     "0000001011111101", "1000101111100001", "0011101000011001", "0100000000000000", "0111111111111111", "0100000000000000",
     others => (others => '0')); --coefficient memory in order: s1, a2, a3, b1, b2, b3
 signal wdata_sample, wdata_coeff, rdata_sample, rdata_coeff : signed(c_data_w-1 downto 0) := (others => '0'); --read and write signals
-signal waddr_sample, raddr_sample : unsigned(c_len_sample_mem-1 downto 0);
-signal waddr_coeff, raddr_coeff : unsigned(c_len_coeff_mem-1 downto 0);
+signal waddr_sample, raddr_sample : unsigned(c_len_sample_mem-1 downto 0) := (others => '0');
+signal waddr_coeff, raddr_coeff : unsigned(c_len_coeff_mem-1 downto 0) := (others => '0');
 signal we_sample_mem, we_coeff_mem : std_logic;
 signal new_delay, result : signed(c_data_w-1 downto 0);
-signal state, next_state : t_state := idle;
+signal state, next_state : t_state := idle; 
+signal RQ_c, RQ_s, GNT_c, RDY_c : std_logic := '0'; 
 -----------------------------------------------------------------------------------
 begin
     p_main: process (clk, rst)
@@ -56,21 +59,60 @@ begin
                 new_delay <= (others => '0');
                 result <= (others => '0');
                 state <= idle;
+                output <= (others => '0');
+                RQ_c <= '0';
+                RQ_s <= '0'; 
             elsif rising_edge(clk) then
-            
+                state <= next_state;
+                output <= std_logic_vector(rdata_sample);
+                GNT <= GNT_c;
+                RDY <= RDY_c;
+                --data sync;
+                RQ_c <= RQ;
+                RQ_s <= RQ_c;      
             end if;
         end process;
 
-    p_fsm: process (state)
+    p_fsm: process (state, RQ_s)
         begin
-
+            GNT_c <= '0';
+            RDY_c <= '0';
+            en  <= '0';
+            we_sample_mem <= '0';
+            --rdata_sample <= (others => '0');
+            --wdata_sample <= (others => '0');
+            --raddr_sample <= (others => '0');
+            --waddr_sample <= (others => '0'); 
             case (state) is
                 when idle =>
-
+                    RDY_c <= '0';
+                    GNT_c <= '0';
+                    we_sample_mem <= '0';
+                    waddr_sample <= to_unsigned(1,waddr_sample'length); --set write address
+                    wdata_sample <= signed(input); --set data to be writen to
+                    if RQ_s = '1' then --data valid
+                        GNT_c <= '1';
+                        we_sample_mem <= '1';
+                        next_state <= write; --go to write
+                    else
+                        next_state <= idle;
+                    end if;
                 when write =>
-
+                    GNT_c <= '0';
+                    we_sample_mem <= '0';
+                    if RQ_s = '0' then
+                        raddr_sample <= waddr_sample;
+                        next_state <= read;
+                    else
+                        next_state <= write;
+                    end if;
                 when  read =>
-                
+                    RDY_c <= '1';
+                    next_state <= idle;
+
+                when others =>
+                    next_state <= idle;
+
             end case;
         end process;
 
